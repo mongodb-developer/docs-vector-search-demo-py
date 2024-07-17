356487ac5907d7ecf3edabe3fa9a9d28cf6f55ce
Pavel Duchovny <pash10g@gmail.com>
2024-07-17 16:18:45 +0300

    migrate to chainlit

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-import time
-import traceback
-import gradio as gr
+
 import os
-import asyncio
 from pymongo import MongoClient
 #from langchain_mongodb import MongoDBAtlasVectorSearch
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
 
-output_parser = StrOutputParser()
-
+import chainlit as cl
 import json
 
 
@@ -21,38 +19,42 @@ db_name = 'docs'
 collection_name = 'embeddings'
 collection = client[db_name][collection_name]
 
-try:
-   
-    
-    llm = ChatOpenAI(model='gpt-3.5-turbo',temperature=0)
-    prompt = ChatPromptTemplate.from_messages([
-    ("system", "AI Chatbot"),
-    ("user", "Answer user query {currentMessageContent}")])
-    
-    chain = prompt | llm | output_parser
 
-except Exception as e:
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Ask a question",
+            message="How do I use FancyWidget?"
+        )
+    ]
 
-    vector_store = None
-    print("An error occurred: \n" + traceback.format_exc())
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a chatbot.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
-def get_chat_response(message, history):
 
-    try:
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
 
-        print_llm_text = chain.invoke({"vectorSearch": input, "currentMessageContent": message, "history": str(history) })
-    
-        for i in range(len(print_llm_text)):
-            time.sleep(0.05)
-            yield  print_llm_text[: i+1]
-    except Exception as e:
-        error_message = traceback.format_exc()
-        print("An error occurred: \n" + error_message)
-        yield "Please clone the repo and add your open ai key as well as your MongoDB Atlas URI in the Secret Section of you Space\n OPENAI_API_KEY (your Open AI key) and MONGODB_ATLAS_URI (0.0.0.0/0 whitelisted instance with Vector index created) \n\n For more information : https://mongodb.com/products/platform/atlas-vector-search"
-        
-    
+    msg = cl.Message(content="")
 
-demo = gr.ChatInterface(get_chat_response, examples=["How to install the FancyWidget?"], title="FancyWidget chatbot",description="This small chat uses a similarity search to find relevant docs, it uses MongoDB Atlas Vector Search read more here: https://www.mongodb.com/docs/atlas/atlas-vector-search/vector-search-tutorial",submit_btn="Search").queue()
+    for chunk in await cl.make_async(runnable.stream)(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
 
-if __name__ == "__main__":
-    demo.launch()
+    await msg.send()
